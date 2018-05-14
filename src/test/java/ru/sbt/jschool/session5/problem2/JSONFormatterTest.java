038be598012a6b8c436d1332373a692bba1f58a5
nastymazurova <nasty.mazurova@gmail.com>
2018-05-14 15:51:16 +0300

    session-5

--- src/test/java/ru/sbt/jschool/session5/problem2/JSONFormatterTest.java
+++ src/test/java/ru/sbt/jschool/session5/problem2/JSONFormatterTest.java
@@ -8,29 +8,62 @@ import static org.junit.Assert.assertEquals;
  */
 public class JSONFormatterTest {
     JSONFormatterImpl jsonFormatter = new JSONFormatterImpl();
-
-    @Test public void testOne() {
+    @Test public void testOne() throws IllegalAccessException {
         A a = new A();
 
         try {
             assertEquals("{\n" +
-                    "    \"fromAObject\" : {\n" +
-                    "        \"aFromB\" : 45,\n" +
-                    "        \"strFromC\" : Hi, I'm from class \"C\"\n" +
-                    "    },\n" +
-                    "\n" +
-                    "    \"array\" : [ \n" +
+                    "    \"dArray\" : [\n" +
+                    "        {\n" +
+                    "            \"i\" : 0,\n" +
+                    "            \"str\" : hi\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"i\" : 1,\n" +
+                    "            \"str\" : hi\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"i\" : 2,\n" +
+                    "            \"str\" : hi\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"i\" : 3,\n" +
+                    "            \"str\" : hi\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"i\" : 4,\n" +
+                    "            \"str\" : hi\n" +
+                    "        }\n" +
+                    "    ],\n" +
+                    "    \"strArray\" : [\n" +
+                    "        {\n" +
+                    "            \"Hello\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"I'm\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"strArray\"\n" +
+                    "        }\n" +
+                    "    ],\n" +
+                    "    \"charArray\" : [ \n" +
                     "        \"5\",\n" +
                     "        \"1\",\n" +
                     "        \"a\"\n" +
                     "    ],\n" +
-                    "\n" +
+                    "    \"intArray\" : [ \n" +
+                    "        \"1\",\n" +
+                    "        \"2\",\n" +
+                    "        \"3\"\n" +
+                    "    ],\n" +
                     "    \"a\" : 5,\n" +
-                    "\n" +
+                    "    \"fromAObject\" : \n" +
+                    "    {\n" +
+                    "        \"aFromB\" : 45\n" +
+                    "        \"strFromC\" : Hi, I'm from class \"C\"\n" +
+                    "    },\n" +
                     "    \"b\" : Hello,\n" +
-                    "\n" +
-                    "    \"date\" : 17.04.2018,\n" +
-                    "\n" +
+                    "    \"date\" : 14.05.2018,\n" +
                     "    \"integerList\" : [ \n" +
                     "        \"5\",\n" +
                     "        \"15\",\n" +
